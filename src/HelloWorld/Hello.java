09d6cf0a57d77533c59ef1e85862b8c7dce78467
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-04-07 17:59:31 -0500

    myFirstNumber

--- src/HelloWorld/Hello.java
+++ src/HelloWorld/Hello.java
@@ -2,6 +2,9 @@ package HelloWorld;
 
 public class Hello {
     public static void main(String[] args){
-        System.out.println("Hello Ryan");
+        System.out.println("Hello, Ryan");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        System.out.println(myFirstNumber);
     }
 }
