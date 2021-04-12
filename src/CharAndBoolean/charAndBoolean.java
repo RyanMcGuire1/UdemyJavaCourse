161fc2fbe9415b7c883aa552091307943807533b
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-04-12 15:31:20 -0500

    Strings

---
+++ src/CharAndBoolean/charAndBoolean.java
@@ -0,0 +1,18 @@
+package CharAndBoolean;
+
+public class charAndBoolean {
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicode = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicode);
+        char myCopyright = '\u00a9';
+        System.out.println(myCopyright);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+    }
+}
