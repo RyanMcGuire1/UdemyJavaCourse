2c8420631e16fe1dd43a518d863f091f9d75f5c5
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-04-14 09:52:53 -0500

    Ternary Operator

--- src/Operators/Operators.java
+++ src/Operators/Operators.java
@@ -42,9 +42,46 @@ public class Operators {
         result /= 3; //30 / 3 = 10
         System.out.println("30 / 3 = " + result);
 
+
         //result = result - 2
         result -= 2; // 10 - 2 = 8
         System.out.println("10 - 2 = " + result);
 
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
+
+        int topScore = 80;
+        if (topScore < 100){
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 95;
+        if((topScore > secondTopScore) && (topScore < 100)){
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+        if ((topScore > 90)  || (secondTopScore <= 90)){
+            System.out.println("Either or both of the conditions are true");
+        }
+
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is true");
+        }
+
+    boolean isCar = false;
+        if (isCar){
+            System.out.println("This is not supposed to happen");
+        }
+
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar) {
+            System.out.println("wasCar is true");
+        }
     }
+
 }
