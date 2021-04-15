9a59e2765525a64189246a459bcd106db09f699a
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-04-15 18:30:06 -0500

    Keywords and Expressions

---
+++ src/KeywordsAndExpressions/keywordsAndExpressions.java
@@ -0,0 +1,13 @@
+package KeywordsAndExpressions;
+
+public class keywordsAndExpressions {
+    public static void main(String[] args) {
+        // a mile = 1.609344 kilometers
+        double kilometers = (100 * 1.609344);
+        int highScore = 50;
+
+        if (highScore ==50){
+            System.out.printf("This is an expression");
+        }
+    }
+}
