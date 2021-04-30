28ffb41fa571d3703c82a95e435d580bdc93c305
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-04-29 22:09:56 -0500

    Minutes to years and days calculator exercise

---
+++ src/MinutesToYearsandDaysCalculator/Main.java
@@ -0,0 +1,11 @@
+package MinutesToYearsandDaysCalculator;
+
+public class Main {
+    public static void main(String[] args) {
+        MinutesToYearsDaysCalculator.printYearsAndDays(-525600);
+        MinutesToYearsDaysCalculator.printYearsAndDays(1051200);
+        MinutesToYearsDaysCalculator.printYearsAndDays(561600);
+
+
+    }
+}
