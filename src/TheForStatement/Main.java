be683ad8fd7c7031a1081f3752bcaf9463e9acfe
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-05-07 16:15:35 -0500

    Sum 3 and 5 challenge

--- src/TheForStatement/Main.java
+++ src/TheForStatement/Main.java
@@ -2,47 +2,61 @@ package TheForStatement;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0,2.0));
-        System.out.println("10,000 at 3% interest = " + calculateInterest(10000.0,3.0));
-        System.out.println("10,000 at 4% interest = " + calculateInterest(10000.0,4.0));
-        System.out.println("10,000 at 5% interest = " + calculateInterest(10000.0,25.0));
+        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
+        System.out.println("10,000 at 3% interest = " + calculateInterest(10000.0, 3.0));
+        System.out.println("10,000 at 4% interest = " + calculateInterest(10000.0, 4.0));
+        System.out.println("10,000 at 5% interest = " + calculateInterest(10000.0, 25.0));
 
 
-        for(int i=0; i<5; i++){
+        for (int i = 0; i < 5; i++) {
             System.out.println("Loop " + i + " hello!");
         }
-        for(int i=2; i<9; i++){
-            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f",calculateInterest(10000.0, i)));
+        for (int i = 2; i < 9; i++) {
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)));
         }
         System.out.println("**************************************");
-        for(int i=8; i>1; i--){
-            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f",calculateInterest(10000.0, i)));
+        for (int i = 8; i > 1; i--) {
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)));
         }
         int count = 0;
-        for (int i=10; i<50; i++){
-            if(isPrime(i)){
+        for (int i = 10; i < 50; i++) {
+            if (isPrime(i)) {
                 count++;
                 System.out.println("The number " + i + " is prime");
-                if(count == 10){
+                if (count == 10) {
                     System.out.println("Exiting for loop");
                     break;
                 }
             }
         }
+        int howMany = 0;
+        int sum = 0;
+        for (int i = 1; i <= 1000; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                howMany++;
+                sum += i;
+                System.out.println("found number = " + i);
+                if (count == 5) {
+                    break;
+                }
+            }
+            System.out.println("Sum = " + sum);
+        }
     }
-    public static boolean isPrime(int n){
-        if(n == 1){
+
+    public static boolean isPrime(int n) {
+        if (n == 1) {
             return false;
         }
-        for(int i=2; i <=(long) Math.sqrt(n); i++){
-            if(n % i == 0){
+        for (int i = 2; i <= (long) Math.sqrt(n); i++) {
+            if (n % i == 0) {
                 return false;
             }
         }
         return true;
     }
 
-    public static double calculateInterest(double amount, double interestRate){
-        return(amount * (interestRate/100));
+    public static double calculateInterest(double amount, double interestRate) {
+        return (amount * (interestRate / 100));
     }
 }
