0da4f5325a09a97dd2704c547adb31a15c3e06a5
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-05-14 22:35:55 -0500

    Number Palindrome Challenge

---
+++ src/NumberPalindrome/main.java
@@ -0,0 +1,9 @@
+package NumberPalindrome;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println(NumberPalindrome.isPalindrome(-1221));
+        System.out.println(NumberPalindrome.isPalindrome(707));
+        System.out.println(NumberPalindrome.isPalindrome(11212));
+    }
+}
