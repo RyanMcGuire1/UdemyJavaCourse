2a599f211685c183daa2ee75758ccd5c298cc948
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-05-17 20:40:01 -0500

    First Last Digit Sum

---
+++ src/FirstLastDigitSum/main.java
@@ -0,0 +1,12 @@
+package FirstLastDigitSum;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(252));
+        System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(257));
+        System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(0));
+        System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(5));
+        System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(-10));
+
+    }
+}
