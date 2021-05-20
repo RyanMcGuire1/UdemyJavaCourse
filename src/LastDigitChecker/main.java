4269635ff1806262d57cb24fff3fde2dd0ca607d
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-05-19 22:59:47 -0500

    Last Digit Checker challege

---
+++ src/LastDigitChecker/main.java
@@ -0,0 +1,10 @@
+package LastDigitChecker;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println(LastDigitChecker.hasSameLastDigit(41,22,71));
+        System.out.println(LastDigitChecker.hasSameLastDigit(23,32,42));
+        System.out.println(LastDigitChecker.hasSameLastDigit(9,99,999));
+
+    }
+}
