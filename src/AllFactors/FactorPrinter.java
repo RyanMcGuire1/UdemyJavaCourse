ea2deba79537b11e2946e9631acc66e8f188a70f
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-05-20 11:07:00 -0500

    Factor printer challenge

---
+++ src/AllFactors/FactorPrinter.java
@@ -0,0 +1,10 @@
+package AllFactors;
+
+public class FactorPrinter {
+    public static void printFactors(int number){
+        if(number<1) System.out.print("Invalid Value");
+        for(int i=1; i<=number; i++){
+            if(number%i==0) System.out.print(i + " ");
+        }
+    }
+}
