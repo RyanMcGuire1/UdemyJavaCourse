52a04f064afa97c05fcb17a3da38eb8bc9d51d0e
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-05-21 23:09:07 -0500

    Perfect number challenge

---
+++ src/PerfectNumber/main.java
@@ -0,0 +1,11 @@
+package PerfectNumber;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println(PerfectNumber.isPerfectNumber(6));
+        System.out.println(PerfectNumber.isPerfectNumber(28));
+        System.out.println(PerfectNumber.isPerfectNumber(5));
+        System.out.println(PerfectNumber.isPerfectNumber(-1));
+
+    }
+}
