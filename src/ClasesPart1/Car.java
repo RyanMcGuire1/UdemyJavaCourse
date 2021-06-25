45f4eaa2306be11dff2fd320fd1243d39b40363f
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-06-25 12:33:51 -0500

    Classes Part 1

---
+++ src/ClasesPart1/Car.java
@@ -0,0 +1,12 @@
+package ClasesPart1;
+
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+
+
+}
---
+++ src/ClasesPart1/Main.java
@@ -0,0 +1,9 @@
+package ClasesPart1;
+
+public class Main {
+    public static void main(String[] args) {
+        Car porsche = new Car();
+        Car holden = new Car();
+
+    }
+}
