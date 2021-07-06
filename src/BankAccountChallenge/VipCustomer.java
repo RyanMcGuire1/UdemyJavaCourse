ba485c939749ecff06447cd638f8ed9721101371
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-07-06 16:48:01 -0500

    constructors and bank account challenge

---
+++ src/BankAccountChallenge/VipCustomer.java
@@ -0,0 +1,10 @@
+package BankAccountChallenge;
+
+public class VipCustomer {
+    private String name;
+    private String creditLimit;
+    private String email;
+
+    public VipCustomer(){
+    }
+}
