271fb014f50387b3f48c0e443fb6387b252a55ce
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-08-06 12:42:59 -0500

    private fiealds, constructor with validation

--- src/Encapsulation/EnhancedPlayer.java
+++ src/Encapsulation/EnhancedPlayer.java
@@ -2,6 +2,15 @@ package Encapsulation;
 
 public class EnhancedPlayer {
     private String name;
-    private String health;
+    private int health = 100;
     private String  weapon;
+
+    public EnhancedPlayer(String name, int health, String weapon) {
+        this.name = name;
+
+        if(health >0 && health <= 200){
+            this.health = health;
+        }
+        this.weapon = weapon;
+    }
 }
