97f75f2a071dfb42acf8d88c4707f736de2dedf8
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-08-07 13:16:34 -0500

    Printer class created with addToner method

---
+++ src/EncapsulationChallenge/Printer.java
@@ -0,0 +1,30 @@
+package EncapsulationChallenge;
+
+public class Printer {
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        if(tonerLevel >-1 && tonerLevel <=100){
+           this.tonerLevel = tonerLevel;
+        } else {
+            this.tonerLevel = -1;
+        }
+        this.tonerLevel = tonerLevel;
+        this.duplex = duplex;
+        this.pagesPrinted = 0;
+    }
+
+    public int  addToner(int tonerAmount){
+        if(tonerAmount >0  && tonerAmount <=100){
+           if(this.tonerLevel + tonerAmount >100){
+               return -1;
+           }
+           this.tonerLevel += tonerAmount;
+           return this.tonerLevel;
+        }else{
+            return -1;
+        }
+    }
+}
