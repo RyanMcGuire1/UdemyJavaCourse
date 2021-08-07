d05f988c38ecf92cf398c20a83c18d633ea1c08f
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-08-07 13:22:15 -0500

    printPages method addded to Printer class

--- src/EncapsulationChallenge/Printer.java
+++ src/EncapsulationChallenge/Printer.java
@@ -27,4 +27,19 @@ public class Printer {
             return -1;
         }
     }
+
+    public int printPages(int pages){
+      int pagesToPrint = pages;
+        if (this.duplex) {
+            pagesToPrint = (pages / 2)+(pages % 2);
+            System.out.println("Printing in duplex mode");
+        }
+        this.pagesPrinted += pagesToPrint;
+        return pagesToPrint;
+        }
+
+    public int getPagesPrinted() {
+        return pagesPrinted;
+    }
 }
+
