78bc7b14cbe77f375290bf2ec5446dfb9b3efd95
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-08-11 12:51:51 -0500

    Polymorphism lesson complete

--- src/Polymorphism/Main.java
+++ src/Polymorphism/Main.java
@@ -10,6 +10,10 @@ class Movie{
     public String plot(){
         return "No plot here";
     }
+
+    public String getName() {
+        return name;
+    }
 }
 
 class Jaws extends Movie{
@@ -17,7 +21,7 @@ class Jaws extends Movie{
         super("Jaws");
     }
 
-    public String pot(){
+    public String plot(){
         return "A shark eats lots of people";
     }
 }
@@ -44,8 +48,8 @@ class MazeRunner extends Movie {
     }
 }
 
-class StarWrs extends Movie {
-    public StarWrs() {
+class StarWars extends Movie {
+    public StarWars() {
         super("Star Wars");
     }
 
@@ -59,9 +63,38 @@ class Forgetable extends Movie {
     public Forgetable() {
         super("Forgetable");
     }
+    //No plot method
 }
+
 public class Main {
     public static void main(String[] args) {
 
+      for (int i = 1; i<11; i++){
+         Movie movie = randomMovie();
+          System.out.println("Movie #" + i + " : " + movie.getName() + "\n" + "Plot: " +movie.plot() + "\n");
+      }
+    }
+
+    public static Movie randomMovie(){
+        int randomNumber = (int) (Math.random() * 5) +1;
+        System.out.println("Random number generated was: " + randomNumber);
+        switch (randomNumber){
+            case 1:
+                return new Jaws();
+
+            case 2:
+                return new IndependenceDay();
+
+            case 3:
+                return new MazeRunner();
+
+            case 4:
+                return new StarWars();
+
+            case 5:
+                return new Forgetable();
+        }
+
+        return null;
     }
 }
