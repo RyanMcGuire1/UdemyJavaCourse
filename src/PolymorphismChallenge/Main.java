0dd475b9c7a7f1e679ee7b7da08f966c45ad2353
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-08-16 16:28:57 -0500

    Messed up, have to redo about half

---
+++ src/PolymorphismChallenge/Main.java
@@ -0,0 +1,103 @@
+package PolymorphismChallenge;
+
+class Car{
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+
+    public Car(int cylinders, String name) {
+        this.cylinders = cylinders;
+        this.name = name;
+        this.wheels = 4;
+        this.engine = true;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String startEngine(){
+        return "Car -> startEngine()";
+    }
+
+    public String accelerate(){
+        return "Car -> accelerate()";
+    }
+
+    public String brake(){
+        return "Car -> brake()";
+    }
+}
+
+class Mitsubishi extends Car{
+
+    public Mitsubishi(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Mitsubishi -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Mitsubishi -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Mitsubishi -> brake()";
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        //We are going to go back to the car analogy.
+        //Create a bas class call car
+        //It should have a few fields that would be appropriate for a generic car class.
+        //engine, cylinders, wheels, etc.
+        //Constructor should initialize cylinders (number of) and name, and set wheels to 4
+        //and engine to true. Cylinders and names would be pass parameters.
+        //
+        //Create appropriate getters
+        //
+        //create some methods like startEngine, accelerate, and brake
+        //
+        //show a message for each in the base class
+        //Now create 3 sub classes for your favorite vehicles.
+        //Override the appropriate methods to demonstrate polymorphism in use.
+        //put all classes in the one java file (this one)
+
+        Car car = new Car(8,"base car");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+
+        Mitsubishi mitsubishi = new Mitsubishi(6, "Outlander VRW 4WD");
+        System.out.println(mitsubishi.startEngine());
+        System.out.println(mitsubishi.accelerate());
+        System.out.println(mitsubishi.brake());
+
+        Ford ford = new Ford(6, "Ford Falcon");
+        System.out.println(ford.startEngine());
+        System.out.println(ford.accelerate());
+        System.out.println(ford.brake());
+
+        Mitsubishi mitsubishi = new Mitsubishi(6, "Outlander VRW 4WD");
+        System.out.println(mitsubishi.startEngine());
+        System.out.println(mitsubishi.accelerate());
+        System.out.println(mitsubishi.brake());Mitsubishi mitsubishi = new Mitsubishi(6, "Outlander VRW 4WD");
+        System.out.println(mitsubishi.startEngine());
+        System.out.println(mitsubishi.accelerate());
+        System.out.println(mitsubishi.brake());Mitsubishi mitsubishi = new Mitsubishi(6, "Outlander VRW 4WD");
+        System.out.println(mitsubishi.startEngine());
+        System.out.println(mitsubishi.accelerate());
+        System.out.println(mitsubishi.brake());
+    }
+
+}
