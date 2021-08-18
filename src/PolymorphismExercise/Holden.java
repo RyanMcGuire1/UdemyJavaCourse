fc5c57e95afec717e1cf04b6ca75e617cd97ee7c
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-08-17 22:19:48 -0500

    Polymorphism exorcise Holden.java

--- src/PolymorphismExercise/Holden.java
+++ src/PolymorphismExercise/Holden.java
@@ -1,2 +1,25 @@
-package PolymorphismExercise;public class Holden {
+package PolymorphismExercise;
+
+public class Holden extends Car{
+    public Holden (int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+     @Override
+     public String startEngine() {
+         // return " Holden -> startEngine()";
+         return getClass().getSimpleName() + " -> startEngine()";
+     }
+
+     @Override
+     public String accelerate() {
+         // return "Holden -> accelerate()";
+         return getClass().getSimpleName() + " -> accelerate()";
+     }
+
+     @Override
+     public String brake() {
+         // return "Holden -> brake()";
+         return getClass().getSimpleName() + " -> brake()";
+     }
 }
