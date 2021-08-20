9b68251a190d466bbf5d33725ce6f5b7cfac947b
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-08-19 23:32:26 -0500

    constructor and additional properties added

--- src/Operators/Hamburger.java
+++ src/Operators/Hamburger.java
@@ -5,4 +5,23 @@ public class Hamburger {
     private String meat;
     private double price;
     private String breadRollType;
+
+    private String addition1Name;
+    private double addition1Price;
+
+    private String addition2Name;
+    private double addition2Price;
+
+    private String addition3Name;
+    private double addition3Price;
+
+    private String addition4Name;
+    private double addition4Price;
+
+    public Hamburger(String name, String meat, double price, String breadRollType) {
+        this.name = name;
+        this.meat = meat;
+        this.price = price;
+        this.breadRollType = breadRollType;
+    }
 }
